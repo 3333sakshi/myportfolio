e74750d4533a0a5498ba74327980231c8dedefd9
Sakshi Tiwari <118250768+3333sakshi@users.noreply.github.com>
2025-07-19 11:08:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var sidemen=document.getElementById("sidemenu");
+    function openmenu(){
+        sidemen.style.right="0";
+    }
+    function openmenu(){
+        sidemen.style.right="-200px";
+    }
